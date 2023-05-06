8e992b9c160b00a93ba514216d50617af2b94cbe
Dmitry <dmitry@mail.ru>
2023-05-06 23:11:06 +0700

    add blog

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,9 @@
         <a href="/" class="nav-logo" title="Back to Homepage">Имя Фамилия</a>
         <div class="nav-links">
             {#each nav as link}
-            <a href={link.href} class="link">{link.title}</a>
+				{#if link.isShow}
+				<a href={link.href} class="link">{link.title}</a>
+				{/if}
             {/each}
         </div>
     </div>
@@ -17,23 +19,28 @@
     	const nav = [
 		{
 			title: 'Обо мне',
-			href: '/about'
+			href: '/about',
+			isShow: true
 		},
 		{
 			title: 'Блог',
-			href: '/blog'
+			href: '/blog',
+			isShow: true
 		},
 		{
 			title: 'Портфолио',
-			href: '/project'
+			href: '/project',
+			isShow: true
 		},
 		{
 			title: 'Контакты',
-			href: '/contact'
+			href: '/contact',
+			isShow: true
 		},
         {
 			title: 'temp',
-			href: '/temp'
+			href: '/temp',
+			isShow: true
 		}
 	]
 </script>
